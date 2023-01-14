319c30064c9c799bdccaf632c6131dfe923e1425
Gio <gianni.boscarino@gmail.com>
2023-01-14 16:30:26 +0100

    fixed byid query + added update

--- Controllers/FeatureFlagController.cs
+++ Controllers/FeatureFlagController.cs
@@ -9,31 +9,40 @@ namespace Redis.OM.Skeleton.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class FeatureFlagController : ControllerBase
+public class FeatureFlagsController : ControllerBase
 {
 
 	private readonly RedisCollection<FeatureFlag> _featureFlags;
 	private readonly RedisConnectionProvider _provider;
-	public FeatureFlagController(RedisConnectionProvider provider)
+	public FeatureFlagsController(RedisConnectionProvider provider)
 	{
 		_provider = provider;
 		_featureFlags = (RedisCollection<FeatureFlag>)provider.RedisCollection<FeatureFlag>();
 	}
 
 	[HttpPost]
-	public async Task<FeatureFlag> AddFeatureFlag([FromBody] FeatureFlag ff)
+	public async Task<FeatureFlag> Add([FromBody] FeatureFlag ff)
 	{
 		ff.LastUpdatedOn = DateTime.Now;
 		await _featureFlags.InsertAsync(ff);
 		return ff;
 	}
 
-
-	[HttpGet()]
-	public FeatureFlag FilterByAge(string id)
+	[HttpPut()]
+	public async Task<FeatureFlag> Update(string id, [FromQuery]bool flag)
 	{        
-		return _featureFlags.FirstOrDefault(x => x.Id == id);
+		var ff =  await _featureFlags.FindByIdAsync(id);
+		ff.Active = flag;
+		ff.LastUpdatedOn = DateTime.Now;
+		await _featureFlags.UpdateAsync(ff);
+		return ff;
 	}
 
+	[HttpGet()]
+	public async Task<FeatureFlag> GetById(string id)
+	{        
+		//return _featureFlags.FirstOrDefault(x => x.Id == id);
+		return await _featureFlags.FindByIdAsync(id);
+	}
 
 }
